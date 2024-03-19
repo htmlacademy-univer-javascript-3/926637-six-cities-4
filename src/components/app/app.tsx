7040d5b2c860eba65402a46dec4d8ba0f1c22d2f
Artyom Borisov <2001raf@gmail.com>
2024-03-20 00:09:34 +0500

    Add app

---
+++ src/components/app/app.tsx
@@ -0,0 +1,14 @@
+import MainPage from '../../pages/main/main';
+
+type AppProps = {
+	offersCount: number;
+};
+
+
+function App ({offersCount}: AppProps): JSX.Element {
+  return (
+    <MainPage offersCount={offersCount}/>
+  );
+}
+
+export default App;

46352c1436f36191f59a8f27dfbb2dcc0f44eba5
Artyom Borisov <2001raf@gmail.com>
2024-03-26 03:46:28 +0500

    Add not found page

---
+++ src/pages/not-found/not-found.tsx
@@ -0,0 +1,57 @@
+import { Link } from 'react-router-dom';
+import Logo from '../../components/logo/logo';
+import { AppRoute } from '../../const';
+
+function NotFoundPage(): JSX.Element {
+  return (
+    <div className="page page--gray page--main">
+      <header className="header">
+        <div className="container">
+          <div className="header__wrapper">
+            <div className="header__left">
+              <Logo />
+            </div>
+            <nav className="header__nav">
+              <ul className="header__nav-list">
+                <li className="header__nav-item user">
+                  <a
+                    className="header__nav-link header__nav-link--profile"
+                    href="#"
+                  >
+                    <div className="header__avatar-wrapper user__avatar-wrapper"></div>
+                    <span className="header__user-name user__name">
+                      Oliver.conner@gmail.com
+                    </span>
+                    <span className="header__favorite-count">3</span>
+                  </a>
+                </li>
+                <li className="header__nav-item">
+                  <a className="header__nav-link" href="#">
+                    <span className="header__signout">Sign out</span>
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </header>
+      <main className="page__main page__main--index page__main--index-empty">
+        <div className="cities">
+          <div className="cities__places-container cities__places-container--empty container">
+            <section className="cities__no-places">
+              <div className="cities__status-wrapper tabs__content">
+                <b className="cities__status">Oops... Page not found.</b>
+                <p className="cities__status-description">
+                  <Link to={AppRoute.Main}>Back go the main page</Link>
+                </p>
+              </div>
+            </section>
+            <div className="cities__right-section" />
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default NotFoundPage;

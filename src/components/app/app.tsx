a5e35f384585787d7082a9cfc39905248f91c6d8
Artyom Borisov <2001raf@gmail.com>
2024-03-26 03:47:54 +0500

    Add routing to app component

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,11 @@
 import MainPage from '../../pages/main/main';
+import {AppRoute, AuthStatus} from '../../const';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LoginPage from '../../pages/login/login';
+import FavoritesPage from '../../pages/favorites/favorites';
+import OfferPage from '../../pages/offer/offer';
+import NotFoundPage from '../../pages/not-found/not-found';
+import PrivateRoute from '../private-route/private-route';
 
 type AppProps = {
 	offersCount: number;
@@ -7,7 +14,34 @@ type AppProps = {
 
 function App ({offersCount}: AppProps): JSX.Element {
   return (
-    <MainPage offersCount={offersCount}/>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Main}
+          element={<MainPage offersCount={offersCount} />}
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<LoginPage/>}
+        />
+        <Route
+          path={AppRoute.Favorites}
+          element={
+            <PrivateRoute authStatus={AuthStatus.NoAuth}>
+              <FavoritesPage/>
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path={AppRoute.Offer}
+          element={<OfferPage/>}
+        />
+        <Route
+          path="*"
+          element={<NotFoundPage/>}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 

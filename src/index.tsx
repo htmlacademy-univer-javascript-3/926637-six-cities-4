08a873c07c49fc590f06cfab17034b7e3b3ab955
Artyom Borisov <2001raf@gmail.com>
2024-05-14 00:05:38 +0500

    Pass comments argument in index

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 import { offers } from './mocks/offers';
+import { comments } from './mocks/comments';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -9,6 +10,6 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App offers={offers}/>
+    <App offers={offers} comments={comments}/>
   </React.StrictMode>
 );

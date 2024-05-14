6f1d3357906e874f1b9c07750900708e0f3e5945
Artyom Borisov <2001raf@gmail.com>
2024-05-14 13:46:52 +0500

    Add zoom in mock locations

--- src/mocks/locations.ts
+++ src/mocks/locations.ts
@@ -1,18 +1,24 @@
-export const locations = [
+import { Location } from "../types/location";
+
+export const locations: Location[] = [
   {
     'latitude': 52.3909553943508,
-    'longitude': 4.85309666406198
+    'longitude': 4.85309666406198,
+    'zoom': 1
   },
   {
     'latitude': 52.3609553943508,
-    'longitude': 4.85309666406198
+    'longitude': 4.85309666406198,
+    'zoom': 1
   },
   {
     'latitude': 52.3909553943508,
-    'longitude': 4.929309666406198
+    'longitude': 4.929309666406198,
+    'zoom': 1
   },
   {
     'latitude': 52.3809553943508,
-    'longitude': 4.939309666406198
+    'longitude': 4.939309666406198,
+    'zoom': 1
   },
 ];

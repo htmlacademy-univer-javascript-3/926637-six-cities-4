52991eb580b9d04d81d6d724655dffb1e498384f
Artyom Borisov <2001raf@gmail.com>
2024-05-14 14:14:02 +0500

    Rename mock locations

--- src/mocks/locations.ts
+++ src/mocks/locations.ts
@@ -1,6 +1,6 @@
 import { Location } from "../types/location";
 
-export const locations: Location[] = [
+export const amsterdamOffersLocations: Location[] = [
   {
     'latitude': 52.3909553943508,
     'longitude': 4.85309666406198,

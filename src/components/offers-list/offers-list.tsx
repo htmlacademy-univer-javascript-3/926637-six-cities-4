3c9b31f9c2c979dcfaada7f44e59859ff46e54f2
Artyom Borisov <2001raf@gmail.com>
2024-05-14 15:55:07 +0500

    Add setActiveOffer for OffersList

--- src/components/offers-list/offers-list.tsx
+++ src/components/offers-list/offers-list.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Offer } from '../../types/offer';
 import OfferCard from '../offer-card/offer-card';
 import {OfferCardType} from '../../const';
@@ -6,21 +5,17 @@ import {OfferCardType} from '../../const';
 type OffersListProps = {
   offers: Offer[];
   offerCardType: OfferCardType;
+  setActiveOffer: (offer: Offer | null) => void
 };
 
-function OffersList({ offers, offerCardType }: OffersListProps) {
-  const [activeOffer, setActiveOffer] = React.useState<Offer | null>(null);
+function OffersList({ offers, offerCardType, setActiveOffer }: OffersListProps) {
   const favoritesCitiesNames = [...new Set(offers.map((offer) => offer.city.name))];
-  if (activeOffer === null){
-    offers = [...offers];
-  }
-  // setActiveOffer(activeOffer);
   switch (offerCardType){
     case OfferCardType.Main:
       return (
         <div className='cities__places-list places__list tabs__content'>
           {offers.map((offer) => (
-            <OfferCard offer={offer} offerCardType={offerCardType} key={offer.id} onMouseOver={() => setActiveOffer(offer)}/>
+            <OfferCard offer={offer} offerCardType={offerCardType} key={offer.id} onMouseOver={() => setActiveOffer(offer)} onMouseLeave={() => setActiveOffer(null)}/>
           ))}
         </div>
       );
@@ -36,7 +31,7 @@ function OffersList({ offers, offerCardType }: OffersListProps) {
               </div>
               <div className='favorites__places'>
                 {offers.filter((offer) => offer.city.name === city).map((offer) => (
-                  <OfferCard offer={offer} offerCardType={offerCardType} key={offer.id} onMouseOver={() => setActiveOffer(offer)}/>
+                  <OfferCard offer={offer} offerCardType={offerCardType} key={offer.id} onMouseOver={() => setActiveOffer(offer)} onMouseLeave={() => setActiveOffer(null)}/>
                 ))}
               </div>
             </li>

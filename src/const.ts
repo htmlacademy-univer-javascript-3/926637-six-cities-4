54c424c743ab96e91b0b9cc5ec48550173e309f3
Artyom Borisov <2001raf@gmail.com>
2024-05-14 14:14:29 +0500

    Add city to center location map

--- src/const.ts
+++ src/const.ts
@@ -1,3 +1,5 @@
+import { Location } from './types/location';
+
 export enum AppRoute {
 	Main = '/',
 	Login = '/login',
@@ -24,3 +26,36 @@ export enum CityToOffer {
 	Hamburg = 'Hamburg',
 	Dusseldorf = 'Dusseldorf'
 }
+
+export const CityToCenterLocationMap: Record<CityToOffer, Location> = {
+	[CityToOffer.Amsterdam]: {
+		'latitude': 52.37403000,
+		'longitude': 4.88969000,
+		'zoom': 1
+	},
+	[CityToOffer.Cologne]: {
+		'latitude': 52.37403000,
+		'longitude': 4.88969000,
+		'zoom': 1
+	},
+	[CityToOffer.Brussels]: {
+		'latitude': 52.37403000,
+		'longitude': 4.88969000,
+		'zoom': 1
+	},
+	[CityToOffer.Paris]: {
+		'latitude': 52.37403000,
+		'longitude': 4.88969000,
+		'zoom': 1
+	},
+	[CityToOffer.Hamburg]: {
+		'latitude': 52.37403000,
+		'longitude': 4.88969000,
+		'zoom': 1
+	},
+	[CityToOffer.Dusseldorf]: {
+		'latitude': 52.37403000,
+		'longitude': 4.88969000,
+		'zoom': 1
+	},
+};

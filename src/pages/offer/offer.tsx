df6d2523006750012ca8e1bec6ca4502e958be8d
Artyom Borisov <2001raf@gmail.com>
2024-05-16 00:57:28 +0500

    Add header in offer page

--- src/pages/offer/offer.tsx
+++ src/pages/offer/offer.tsx
@@ -8,6 +8,7 @@ import { CityToOffer, OfferCardType } from '../../const';
 import React from 'react';
 import { Offer } from '../../types/offer';
 import Map from '../../components/map/map';
+import { Header } from '../../components/header/header';
 
 type OfferPageProps = {
   comments: Comment[];
@@ -20,36 +21,7 @@ function OfferPage({comments}: OfferPageProps): JSX.Element {
   }
   return (
     <div className="page">
-      <header className="header">
-        <div className="container">
-          <div className="header__wrapper">
-            <div className="header__left">
-              <Logo/>
-            </div>
-            <nav className="header__nav">
-              <ul className="header__nav-list">
-                <li className="header__nav-item user">
-                  <a
-                    className="header__nav-link header__nav-link--profile"
-                    href="#"
-                  >
-                    <div className="header__avatar-wrapper user__avatar-wrapper"></div>
-                    <span className="header__user-name user__name">
-                      Oliver.conner@gmail.com
-                    </span>
-                    <span className="header__favorite-count">3</span>
-                  </a>
-                </li>
-                <li className="header__nav-item">
-                  <a className="header__nav-link" href="#">
-                    <span className="header__signout">Sign out</span>
-                  </a>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-      </header>
+      <Header isActive={false}/>
       <main className="page__main page__main--offer">
         <section className="offer">
           <div className="offer__gallery-container container">
@@ -185,7 +157,7 @@ function OfferPage({comments}: OfferPageProps): JSX.Element {
               </section>
             </div>
           </div>
-          <Map city={CityToOffer.Amsterdam} offers={offers.slice(0, 3)} activeOffer={activeOffer} type="offer"/>
+          <Map offers={offers.slice(0, 3)} activeOffer={activeOffer} type="offer"/>
         </section>
         <div className="container">
           <section className="near-places places">

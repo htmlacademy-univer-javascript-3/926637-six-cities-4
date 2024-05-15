85bc4e5fe9ab446c79f786b5144428b7d3c7926b
Artyom Borisov <2001raf@gmail.com>
2024-05-15 12:02:55 +0500

    Use offers sorting form in main page

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -6,11 +6,13 @@ import React from 'react';
 import Map from '../../components/map/map';
 import CitiesList from '../../components/cities-list/cities-list';
 import { useAppSelector } from '../../hooks';
+import OffersSortingForm from '../../components/offers-sorting-form/offers-sorting-form';
 
 
 function MainPage(): JSX.Element {
   const [activeOffer, setActiveOffer] = React.useState<Offer | null>(null);
-  const offers = useAppSelector((state) => state.offers);
+  const offers = useAppSelector((state) => state.currentOffers);
+  const city = useAppSelector((state) => state.city);
 
   return (
     <div className="page page--gray page--main">
@@ -52,34 +54,9 @@ function MainPage(): JSX.Element {
             <section className="cities__places places">
               <h2 className="visually-hidden">Places</h2>
               <b className="places__found">
-                {offers.length} places to stay in Amsterdam
+                {offers.length} places to stay in {city.toString()}
               </b>
-              <form className="places__sorting" action="#" method="get">
-                <span className="places__sorting-caption">Sort by</span>
-                <span className="places__sorting-type" tabIndex={0}>
-                  Popular
-                  <svg className="places__sorting-arrow" width={7} height={4}>
-                    <use xlinkHref="#icon-arrow-select" />
-                  </svg>
-                </span>
-                <ul className="places__options places__options--custom places__options--opened">
-                  <li
-                    className="places__option places__option--active"
-                    tabIndex={0}
-                  >
-                    Popular
-                  </li>
-                  <li className="places__option" tabIndex={0}>
-                    Price: low to high
-                  </li>
-                  <li className="places__option" tabIndex={0}>
-                    Price: high to low
-                  </li>
-                  <li className="places__option" tabIndex={0}>
-                    Top rated first
-                  </li>
-                </ul>
-              </form>
+              <OffersSortingForm/>
               <OffersList offers={offers} offerCardType={OfferCardType.Main} setActiveOffer={setActiveOffer}/>
             </section>
             <div className="cities__right-section">

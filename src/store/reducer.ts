be4e72705baefb16a0a915f458e87cebbbc95392
Artyom Borisov <2001raf@gmail.com>
2024-05-15 08:15:23 +0500

    Add reducer

---
+++ src/store/reducer.ts
@@ -0,0 +1,27 @@
+import { createReducer } from '@reduxjs/toolkit';
+import { CityToOffer } from '../const';
+import { offers } from '../mocks/offers';
+import { Offer } from '../types/offer';
+import { setCityToOffer, getOffers } from './action';
+
+type StateType = {
+  offers: Offer[];
+  city: CityToOffer;
+}
+
+const initialState: StateType = {
+  offers: offers,
+  city: CityToOffer.Amsterdam
+};
+
+const reducer = createReducer(initialState, (builder) => {
+  builder
+    .addCase(setCityToOffer, (state: StateType, action: { payload: CityToOffer }) => {
+      state.city = action.payload;
+    })
+    .addCase(getOffers, (state: StateType) => {
+      state.offers = offers;
+    });
+});
+
+export {reducer};

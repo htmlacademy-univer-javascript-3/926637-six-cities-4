2e633e41fddd30c06d6f165003d8e88f892f3022
Artyom Borisov <2001raf@gmail.com>
2024-05-15 09:50:28 +0500

    Don't pass offers to main page in app

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -21,7 +21,7 @@ function App ({offers, comments}: AppProps): JSX.Element {
       <Routes>
         <Route
           path={AppRoute.Main}
-          element={<MainPage offers={offers} />}
+          element={<MainPage/>}
         />
         <Route
           path={AppRoute.Login}

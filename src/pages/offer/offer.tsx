0d92bb0e37a2eabfe45a3dd8ddf73e6f9ba27433
Artyom Borisov <2001raf@gmail.com>
2024-05-16 22:46:31 +0500

    Update offer page

--- src/pages/offer/offer.tsx
+++ src/pages/offer/offer.tsx
@@ -1,169 +1,53 @@
-import { ReviewForm } from '../../components/review-form/review-form';
-import ReviewList from '../../components/review-list/review-list';
-import { Comment } from '../../types/comment';
-import { offers } from '../../mocks/offers';
 import OffersList from '../../components/offers-list/offers-list';
 import { OfferCardType } from '../../const';
-import React from 'react';
-import { Offer } from '../../types/offer';
+import { useEffect } from 'react';
 import Map from '../../components/map/map';
 import { Header } from '../../components/header/header';
+import { useAppDispatch, useAppSelector } from '../../hooks';
+import { useParams } from 'react-router-dom';
+import { fetchCurrentOffer } from '../../store/api-actions';
+import Loading from '../../components/loading/loading';
+import { OfferCardDetailed } from '../../components/offer-detailed/offer-detailed';
 
-type OfferPageProps = {
-  comments: Comment[];
-}
 
-function OfferPage({comments}: OfferPageProps): JSX.Element {
-  const [activeOffer, setActiveOffer] = React.useState<Offer | null>(null);
-  if (activeOffer !== null && activeOffer.id === '-1'){
-    activeOffer.isFavorite = !!activeOffer.isFavorite; // Pass linter
-  }
+function OfferPage(): JSX.Element {
+  const authStatus = useAppSelector((state) => state.authStatus);
+  const params = useParams();
+  const offerId = params.id;
+  const dispatch = useAppDispatch();
+  const isDoneFetchingCurrentOffer = useAppSelector((state) => state.isDoneFetchingCurrentOffer);
+  const isDoneFetchingClosestOffers = useAppSelector((state) => state.isDoneFetchingClosestOffers);
+  const isDoneFetchingOfferComments = useAppSelector((state) => state.isDoneFetchingOfferComments);
+  const comments = useAppSelector((state) => state.offerComments);
+  const closestOffers = useAppSelector((state) => state.currentClosestOffers);
+  const currentOffer = useAppSelector((state) => state.currentOffer);
+
+  useEffect(() => {
+    if (offerId !== undefined){
+      dispatch(fetchCurrentOffer(offerId));
+    }
+  }, [offerId, dispatch]);
+
   return (
-    <div className="page">
+    <div className='page'>
       <Header isActive={false}/>
-      <main className="page__main page__main--offer">
-        <section className="offer">
-          <div className="offer__gallery-container container">
-            <div className="offer__gallery">
-              <div className="offer__image-wrapper">
-                <img
-                  className="offer__image"
-                  src="img/room.jpg"
-                  alt="Photo studio"
-                />
-              </div>
-              <div className="offer__image-wrapper">
-                <img
-                  className="offer__image"
-                  src="img/apartment-01.jpg"
-                  alt="Photo studio"
-                />
-              </div>
-              <div className="offer__image-wrapper">
-                <img
-                  className="offer__image"
-                  src="img/apartment-02.jpg"
-                  alt="Photo studio"
-                />
-              </div>
-              <div className="offer__image-wrapper">
-                <img
-                  className="offer__image"
-                  src="img/apartment-03.jpg"
-                  alt="Photo studio"
-                />
-              </div>
-              <div className="offer__image-wrapper">
-                <img
-                  className="offer__image"
-                  src="img/studio-01.jpg"
-                  alt="Photo studio"
-                />
-              </div>
-              <div className="offer__image-wrapper">
-                <img
-                  className="offer__image"
-                  src="img/apartment-01.jpg"
-                  alt="Photo studio"
-                />
-              </div>
-            </div>
-          </div>
-          <div className="offer__container container">
-            <div className="offer__wrapper">
-              <div className="offer__mark">
-                <span>Premium</span>
-              </div>
-              <div className="offer__name-wrapper">
-                <h1 className="offer__name">
-                  Beautiful &amp; luxurious studio at great location
-                </h1>
-                <button className="offer__bookmark-button button" type="button">
-                  <svg className="offer__bookmark-icon" width={31} height={33}>
-                    <use xlinkHref="#icon-bookmark" />
-                  </svg>
-                  <span className="visually-hidden">To bookmarks</span>
-                </button>
-              </div>
-              <div className="offer__rating rating">
-                <div className="offer__stars rating__stars">
-                  <span style={{ width: '80%' }} />
-                  <span className="visually-hidden">Rating</span>
-                </div>
-                <span className="offer__rating-value rating__value">4.8</span>
-              </div>
-              <ul className="offer__features">
-                <li className="offer__feature offer__feature--entire">
-                  Apartment
-                </li>
-                <li className="offer__feature offer__feature--bedrooms">
-                  3 Bedrooms
-                </li>
-                <li className="offer__feature offer__feature--adults">
-                  Max 4 adults
-                </li>
-              </ul>
-              <div className="offer__price">
-                <b className="offer__price-value">€120</b>
-                <span className="offer__price-text">&nbsp;night</span>
-              </div>
-              <div className="offer__inside">
-                <h2 className="offer__inside-title">What&aposs inside</h2>
-                <ul className="offer__inside-list">
-                  <li className="offer__inside-item">Wi-Fi</li>
-                  <li className="offer__inside-item">Washing machine</li>
-                  <li className="offer__inside-item">Towels</li>
-                  <li className="offer__inside-item">Heating</li>
-                  <li className="offer__inside-item">Coffee machine</li>
-                  <li className="offer__inside-item">Baby seat</li>
-                  <li className="offer__inside-item">Kitchen</li>
-                  <li className="offer__inside-item">Dishwasher</li>
-                  <li className="offer__inside-item">Cabel TV</li>
-                  <li className="offer__inside-item">Fridge</li>
-                </ul>
-              </div>
-              <div className="offer__host">
-                <h2 className="offer__host-title">Meet the host</h2>
-                <div className="offer__host-user user">
-                  <div className="offer__avatar-wrapper offer__avatar-wrapper--pro user__avatar-wrapper">
-                    <img
-                      className="offer__avatar user__avatar"
-                      src="img/avatar-angelina.jpg"
-                      width={74}
-                      height={74}
-                      alt="Host avatar"
-                    />
-                  </div>
-                  <span className="offer__user-name">Angelina</span>
-                  <span className="offer__user-status">Pro</span>
-                </div>
-                <div className="offer__description">
-                  <p className="offer__text">
-                    A quiet cozy and picturesque that hides behind a a river by
-                    the unique lightness of Amsterdam. The building is green and
-                    from 18th century.
-                  </p>
-                  <p className="offer__text">
-                    An independent House, strategically located between Rembrand
-                    Square and National Opera, but where the bustle of the city
-                    comes to rest in this alley flowery and colorful.
-                  </p>
-                </div>
-              </div>
-              <section className="offer__reviews reviews">
-                <ReviewList comments={comments}/>
-                <ReviewForm/>
-              </section>
-            </div>
-          </div>
-          <Map offers={offers.slice(0, 3)} activeOffer={activeOffer} type="offer"/>
+      <main className='page__main page__main--offer'>
+        <section className='offer'>
+          {
+            (isDoneFetchingCurrentOffer && currentOffer !== null) ? <OfferCardDetailed currentOffer={currentOffer} isDoneFetchingOfferComments={isDoneFetchingOfferComments} authStatus={authStatus} comments={comments}/> : <Loading/>
+          }
+          {
+            (isDoneFetchingClosestOffers && currentOffer !== null) ? <Map centerLocation={currentOffer.city.location} offers={[...closestOffers, currentOffer]} activeOffer={currentOffer} type='offer'/> : <Loading/>
+          }
         </section>
-        <div className="container">
-          <section className="near-places places">
-            <h2 className="near-places__title">
+        <div className='container'>
+          <section className='near-places places'>
+            <h2 className='near-places__title'>
               Other places in the neighbourhood
             </h2>
-            <OffersList offers={offers.slice(0, 3)} offerCardType={OfferCardType.Offer} setActiveOffer={setActiveOffer}/>
+            {
+              isDoneFetchingClosestOffers ? <OffersList offers={closestOffers} offerCardType={OfferCardType.Offer} setActiveOffer={() => {}}/> : ''
+            }
           </section>
         </div>
       </main>

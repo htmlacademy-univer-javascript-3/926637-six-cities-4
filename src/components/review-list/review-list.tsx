a4c2842ca30e0f33df6a1b26a3304807f13a328e
Artyom Borisov <2001raf@gmail.com>
2024-05-16 22:47:30 +0500

    Get 10 newest comment in review list

--- src/components/review-list/review-list.tsx
+++ src/components/review-list/review-list.tsx
@@ -1,19 +1,24 @@
-import {Comment} from '../../types/comment';
+import { useEffect, useState } from 'react';
+import {OfferComment} from '../../types/offer-comment';
 import {Review} from '../review/review';
 
 type ReviewListProps = {
-    comments: Comment[];
+    comments: OfferComment[];
 };
 
 
 function ReviewList ({comments}: ReviewListProps): JSX.Element {
+  const [newestComments, setNewestComments] = useState<OfferComment[]>([]);
+  useEffect(() => {
+    setNewestComments(comments.toSorted((a, b) => (new Date(a.date).getTime() - (new Date(b.date)).getTime())).slice(-10).reverse());
+  }, [comments]);
   return (
     <>
       <h2 className="reviews__title">
         Reviews · <span className="reviews__amount">{comments.length}</span>
       </h2>
       <ul className="reviews__list">
-        {comments.map((comment) => <Review comment={comment} key={comment.id}/>)}
+        {newestComments.map((comment) => <Review comment={comment} key={comment.id}/>)}
       </ul>
     </>
   );
